41f5feae0c7bec78af5a18f26f2f61fe254fd333
ashish4497 <ak8393267@gmail.com>
2024-08-21 18:33:39 +0530

    add Bottom navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import BottomTabNavigator from '@/components/navigation/BottomTabNavigator';
 export default function TabLayout() {
-
   return (
     <BottomTabNavigator/>
   );
--- components/navigation/BottomTabNavigator.tsx
+++ components/navigation/BottomTabNavigator.tsx
@@ -1,22 +1,47 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import HomeScreen from '@/components/Home';
 import ProfileScreen from '@/components/Profile';
-
-
-
-
 
 const Tab = createBottomTabNavigator();
 
 function BottomTabNavigator() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+          switch (route.name) {
+            case 'Home':
+              iconName = 'home';
+              break;
+            case 'Profile':
+              iconName = 'person';
+              break;
+            case 'Settings':
+              iconName = 'settings';
+              break;
+              case 'User':
+                iconName = 'person';
+                break;
+            default:
+              iconName = 'help';
+              break;
+          }
+
+          return <MaterialIcons name={iconName} size={size} color={color} />;
+        },
+      })}
+    >
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen name="User" component={ProfileScreen} />
+      <Tab.Screen name="Settings" component={ProfileScreen} />
     </Tab.Navigator>
   );
 }
 
 export default BottomTabNavigator;
+
 
